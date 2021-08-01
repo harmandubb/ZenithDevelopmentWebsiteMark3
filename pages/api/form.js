572d1234dbc42ee3d0cb74387cc9564404f9ac87
harmandubb <harmand1999@gmail.com>
2021-08-01 12:00:35 -0700

    Chnaged up the strutucture for authorization

--- pages/api/form.js
+++ pages/api/form.js
@@ -12,40 +12,57 @@ export default function handler(req, res) {
 
     console.log("Scope has been set up")
 
-    // const client = new google.auth.JWT(
-    //     keys.client_email,
-    //     null,
-    //     keys.private_key,
-    //     SCOPES
-    // );
-
-    const client = new google.auth.OAuth2(
-        keys.client_id,
+    const client = new google.auth.JWT(
+        keys.client_email,
         null,
-        'token',
+        keys.private_key,
+        keys.private_key_id,
         SCOPES
     );
-    //Seems correction
 
-    console.log("The client property parameters have been set up")
+    // const client = new google.auth.OAuth2(
+    //     keys.client_id,
+    //     null,
+    //     'token',
+    //     SCOPES
+    // );
+    //shoudl check the OAuth2 to see if the parameters are right
+
+    console.log("The client prameters:", client)
 
 
 
     //connecting the client to the google api
-    client.authorize(function (err, tokens) {
-        console.log("authorization has started")
+    // client.authorize(function (err, tokens) {
+    //     console.log("authorization has started")
+
+    //     if (err) {
+    //         console.log("Authenitization of the client has failed")
+    //         console.log(err);
+    //         return;
+    //     } else {
+    //         res.json({ FormSucess: true });
+    //         console.log("1: Pre-Connected");
+    //         gsrun(client, req.body.Name, req.body.Email, req.body.Project, req.body.Message);
+    //         console.log("2: Connected");
+    //     }
+    // });
+
+    client.authorize(function (err, token) {
+        console.log("authorization of the JWT has started");
 
         if (err) {
-            console.log("Authenitization of the client has failed")
+            console.log("Authentication has failed");
             console.log(err);
             return;
         } else {
-            res.json({ FormSucess: true });
-            console.log("1: Pre-Connected");
+            console.log("Token:", token);
+            console.log("Connected!")
             gsrun(client, req.body.Name, req.body.Email, req.body.Project, req.body.Message);
-            console.log("2: Connected");
+            console.log("GSrun has completed.")
         }
     });
+
 
     console.log("Ended the api call to google now")
 
