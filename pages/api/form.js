f92f49eda13f40436ae5e56088b9d999ead9b377
harmandubb <harmand1999@gmail.com>
2021-08-01 12:39:26 -0700

    console log for finding the response from the GSrun function

--- pages/api/form.js
+++ pages/api/form.js
@@ -6,11 +6,8 @@ const today = new Date();
 
 
 export default function handler(req, res) {
-    console.log("Getting to the start of the API function");
 
     const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']; //Check
-
-    console.log("Scope has been set up")
 
     const client = new google.auth.JWT(
         keys.client_email,
@@ -18,18 +15,6 @@ export default function handler(req, res) {
         keys.private_key,
         SCOPES
     );
-
-    // const client = new google.auth.OAuth2(
-    //     keys.client_id,
-    //     null,
-    //     'token',
-    //     SCOPES
-    // );
-    //shoudl check the OAuth2 to see if the parameters are right
-
-    console.log("The client prameters:", client)
-
-
 
     //connecting the client to the google api
     // client.authorize(function (err, tokens) {
@@ -48,39 +33,28 @@ export default function handler(req, res) {
     // });
 
     client.authorize(function (err, token) {
-        console.log("authorization of the JWT has started");
 
         if (err) {
-            console.log("Authentication has failed");
             console.log(err);
             return;
         } else {
-            console.log("Token:", token);
             console.log("Connected!")
-            let output = gsrun(client, req.body.Name, req.body.Email, req.body.Project, req.body.Message);
-            console.log("GSrun has completed.")
-            return output;
+            let gsRes = gsrun(client, req.body.Name, req.body.Email, req.body.Project, req.body.Message);
+            console.log("Response from GSRUN", gsRes);
+            return gsRes;
         }
     });
 
-
-    console.log("Ended the api call to google now")
-
-
     function gsrun(cl, Name, Email, Project, Message) {
-        console.log("GSrun is working")
+
         let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
         let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
         let dateTime = date + '---' + time;
-
-        console.log("DateTime Input:", dateTime)
-
 
         const gsapi = google.sheets({ version: 'v4', auth: cl });
 
         //Data for spreadsheet information
         let inputArray = [[dateTime, Name, Email, Project, Message]];
-
 
         const updateOptions = {
             spreadsheetId: '1b1ZIA939iImXA2wjJsBIbhFR3OF3eaaQuZ8TQPxBDwA',
@@ -89,28 +63,18 @@ export default function handler(req, res) {
             insertDataOption: 'INSERT_ROWS',
             resource: { values: inputArray }
         };
-        console.log("UpdateOptions have been set!")
-
 
         gsapi.spreadsheets.values.append(updateOptions)
             .then((res) => {
-                console.log("Information has been sent out:")
-                console.log("UpdateOptions:", updateOptions)
-                console.log("UpdateOptions:", updateOptions.resource)
+                console.log("Information has been sent out:");
                 console.log("The response:", res);
                 return res;
             })
             .catch((err) => {
-                console.log("The information has not been sent out")
-                console.log("UpdateOptions:", updateOptions.resource)
-                console.error("The following error has occured", err)
+                console.log("The information has not been sent out");
+                console.error("The following error has occured", err);
                 return err;
             });
 
-
-
     }
-
-
 }
-
